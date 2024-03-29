c5fd2bf852edb8460df0577aae92a86885458a77
Aditya Joshi <joshiaditya.aj07@gmail.com>
2024-03-29 21:13:58 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-7xl font-black text-orange-600">Jai Shri Ram</h1>
+    </>
+  );
+}
+
+export default App;
