af1420d647c443bd26d413e4b74f9ebddd8f3554
Aditya Joshi <joshiaditya.aj07@gmail.com>
2024-03-30 15:13:55 +0530

    fix: responsiveness fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="flex flex-col pt-12 items-center h-screen w-[80%] lg:w-[70%] mx-auto ">
+    <div className="flex flex-col pt-12 items-center h-screen w-[95%] lg:w-[60%] mx-auto ">
       <h1 className="text-5xl mb-6 text-white font-black ">Todo App</h1>
       <div className="bg-yellow-400 rounded-lg w-full space-y-4 flex flex-col justify-center items-center p-4 ">
         <TodoForm />
